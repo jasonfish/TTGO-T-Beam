a1b3d3ddd7338643276d51ef0e4734f9d7d91390
Jason Fish <jason@flounder.pond>
2020-08-03 21:40:39 -0700

    Updates to Sender and Reciever

--- OLED_LoRa_Receive/OLED_LoRa_Receive.ino
+++ OLED_LoRa_Receive/OLED_LoRa_Receive.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>  
 #include "SSD1306.h" 
 #include "images.h"
+#include "EasyBuzzer.h"
 
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISO
@@ -10,7 +11,7 @@
 #define SS      18   // GPIO18 -- SX1278's CS
 #define RST     14   // GPIO14 -- SX1278's RESET
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND    868E6
+#define BAND    915E6
 
 SSD1306 display(0x3c, 21, 22);
 String rssi = "RSSI --";
@@ -19,15 +20,21 @@ String packet ;
 
 
 void loraData(){
+
+  unsigned int frequency = 4000;
+  unsigned int beeps = 1;
+  
   display.clear();
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(0 , 15 , "Received "+ packSize + " bytes");
-  display.drawStringMaxWidth(0 , 26 , 128, packet);
-  display.drawString(0, 0, rssi); 
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(64, 0, rssi); 
+//  display.drawString(64 , 20 , "Rec'd "+ packSize + "b");
+  display.drawStringMaxWidth(64 , 40 , 128, packet);
   display.display();
   Serial.println(rssi);
+  EasyBuzzer.beep(frequency, beeps);
 }
+
 
 void cbk(int packetSize) {
   packet ="";
@@ -37,19 +44,21 @@ void cbk(int packetSize) {
   loraData();
 }
 
+
 void setup() {
   pinMode(16,OUTPUT);
   digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
   delay(50); 
   digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
-  
+  EasyBuzzer.setPin(2);
+    
   Serial.begin(115200);
   while (!Serial);
   Serial.println();
   Serial.println("LoRa Receiver Callback");
   SPI.begin(SCK,MISO,MOSI,SS);
-  LoRa.setPins(SS,RST,DI0);  
-  if (!LoRa.begin(868E6)) {
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(915E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
@@ -60,10 +69,22 @@ void setup() {
   display.flipScreenVertically();  
   display.setFont(ArialMT_Plain_10);
    
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(64 , 0 , "Mobile");
+  display.drawString(64 , 20 , "Receiver");
+  display.drawString(64 , 40 , "Ready");
+  display.display();
+  
   delay(1500);
+
 }
 
+
 void loop() {
+  EasyBuzzer.update();
+  LoRa.setSpreadingFactor(12);
   int packetSize = LoRa.parsePacket();
   if (packetSize) { cbk(packetSize);  }
   delay(10);
